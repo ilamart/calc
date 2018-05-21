a7410982d25c83272d07a63b742b5f7b4d470641
ilamart <39222202+ilamart@users.noreply.github.com>
2018-05-21 13:10:31 +0300

    amended

--- calc/Program.cs
+++ calc/Program.cs
@@ -7,24 +7,65 @@ namespace calc
     {
             static void Main(string[] args)
             {
-                while (true)
+            ConsoleKeyInfo es;
+            string input;
+            try
+            {
+                do
                 {
                     Console.Write("Введите выражение: ");
-                    Console.WriteLine(RPN.Calculate(Console.ReadLine()));
+                    RPN rez = new RPN();  
+                    input = Console.ReadLine();
+                    rez.Calculate(input, rez);
+                    Console.WriteLine(rez.result);
+                    Console.WriteLine("ESC для выхода");
+                    es = Console.ReadKey();
+                } while (es.Key != ConsoleKey.Escape);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
             }
         }
-        }
+    }
     class RPN
     {
-        static public double Calculate(string input)
+        Operation op = new Operation();
+        public RPN()
+        {
+
+        }
+        public Nullable<double> result;
+
+        public Nullable<double> Result
+        {
+            set
+            {
+                try
+                {
+                result = value;
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine("Ошибка: " + er.Message);
+                }
+            }
+            get
+            {
+                    return result;
+            }
+        }
+
+        public void Calculate(string input, RPN abc)
         {
             string output = GetExpression(input);
-            double result = Counting(output); 
-            return result; 
+            abc.result = op.Evalute(output);
         }
-        static private string GetExpression(string input)
+           
+        public string GetExpression(string input)
         {
             string output = string.Empty;
+            
             Stack<char> operStack = new Stack<char>();
             for (int i = 0; i < input.Length; i++)
             {
@@ -39,7 +80,7 @@ namespace calc
 
                 if (Char.IsDigit(input[i]))
                 {
-                    while (!Is_Delimeter(input[i]) && !Is_Operator(input[i]))
+                    while (!Is_Delimeter(input[i]) && !(op.operations.IndexOf(input[i]) != -1))
                     {
                         output += input[i];
                         i++;
@@ -49,7 +90,7 @@ namespace calc
                     i--;
                 }
 
-                if (Is_Operator(input[i]))
+                if (op.operations.IndexOf(input[i])!= -1)
                 {
                     if (input[i] == '(')
                         operStack.Push(input[i]); 
@@ -76,7 +117,34 @@ namespace calc
                 output += operStack.Pop() + " ";
             return output;
         }
-        static private double Counting(string output)
+
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                default: return 5;
+            }
+        }
+
+        static private bool Is_Delimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+    }
+
+    public class Operation
+    {
+        public string operations = "+-/*^()";
+
+        public double Evalute(string output)
         {
             string result;
             string[] mas = output.Split(' ');
@@ -118,31 +186,6 @@ namespace calc
                         break;
                 }
             return double.Parse(mas[0]);
-        }
-        static private byte GetPriority(char s)
-        {
-            switch (s)
-            {
-                case '(': return 0;
-                case ')': return 1;
-                case '+': return 2;
-                case '-': return 3;
-                case '*': return 4;
-                case '/': return 4;
-                default: return 5;
-            }
-        }
-        static private bool Is_Operator(char с)
-        {
-            if (("+-/*^()".IndexOf(с) != -1))
-                return true;
-            return false;
-        }
-        static private bool Is_Delimeter(char c)
-        {
-            if ((" =".IndexOf(c) != -1))
-                return true;
-            return false;
         }
     }
 }
