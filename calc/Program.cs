3f32aa5d603eda1e26db116accea88ca6b3e210a
ilamart <39222202+ilamart@users.noreply.github.com>
2018-05-28 19:25:05 +0300

    something new

--- calc/Program.cs
+++ calc/Program.cs
@@ -5,7 +5,7 @@ namespace calc
 {
     class Program
     {
-            static void Main(string[] args)
+        static void Main(string[] args)
             {
             ConsoleKeyInfo es;
             string input;
@@ -14,10 +14,10 @@ namespace calc
                 do
                 {
                     Console.Write("Введите выражение: ");
-                    RPN rez = new RPN();  
+                    StringCalc rez = new StringCalc();
                     input = Console.ReadLine();
-                    rez.Calculate(input, rez);
-                    Console.WriteLine(rez.result);
+                    rez.Calculate(input);
+                    Console.WriteLine(rez._result);
                     Console.WriteLine("ESC для выхода");
                     es = Console.ReadKey();
                 } while (es.Key != ConsoleKey.Escape);
@@ -28,122 +28,21 @@ namespace calc
             }
         }
     }
-    class RPN
-    {
-        Operation op = new Operation();
-        public RPN()
-        {
-
-        }
-        public Nullable<double> result;
-
-        public Nullable<double> Result
-        {
-            set
-            {
-                try
-                {
-                result = value;
-                }
-                catch (Exception er)
-                {
-                    Console.WriteLine("Ошибка: " + er.Message);
-                }
-            }
-            get
-            {
-                    return result;
-            }
-        }
-
-        public void Calculate(string input, RPN abc)
-        {
-            string output = GetExpression(input);
-            abc.result = op.Evalute(output);
-        }
-           
-        public string GetExpression(string input)
-        {
-            string output = string.Empty;
-            
-            Stack<char> operStack = new Stack<char>();
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (Is_Delimeter(input[i]))
-                    continue; 
-
-                if (input[i] == '-' && ((i > 0 && !Char.IsDigit(input[i - 1])) || i == 0))
-                {
-                    i++;
-                    output += "-";  
-                }
-
-                if (Char.IsDigit(input[i]))
-                {
-                    while (!Is_Delimeter(input[i]) && !(op.operations.IndexOf(input[i]) != -1))
-                    {
-                        output += input[i];
-                        i++;
-                        if (i == input.Length) break;
-                    }
-                    output += " ";
-                    i--;
-                }
-
-                if (op.operations.IndexOf(input[i])!= -1)
-                {
-                    if (input[i] == '(')
-                        operStack.Push(input[i]); 
-                    else if (input[i] == ')') 
-                    {
-                        char s = operStack.Pop();
-                        while (s != '(')
-                        {
-                            output += s.ToString() + ' ';
-                            s = operStack.Pop();
-                        }
-                    }
-                    else
-                    {
-                        if (operStack.Count > 0) 
-                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek()))
-                                output += operStack.Pop().ToString() + " ";
-                        operStack.Push(char.Parse(input[i].ToString()));
-                    }
-                }
-            }
-
-            while (operStack.Count > 0)
-                output += operStack.Pop() + " ";
-            return output;
-        }
-
-        static private byte GetPriority(char s)
-        {
-            switch (s)
-            {
-                case '(': return 0;
-                case ')': return 1;
-                case '+': return 2;
-                case '-': return 3;
-                case '*': return 4;
-                case '/': return 4;
-                default: return 5;
-            }
-        }
-
-        static private bool Is_Delimeter(char c)
-        {
-            if ((" =".IndexOf(c) != -1))
-                return true;
-            return false;
-        }
-    }
 
     public class Operation
     {
+
         public string operations = "+-/*^()";
 
+        //public void OperationPlus
+        //{
+        //    result = (double.parse(mas[i - 2]) + double.parse(mas[i - 1])).tostring();
+        //    mas[i - 2] = result;
+        //    for (int j = i - 1; j<mas.length - 2; j++)
+        //        mas[j] = mas[j + 2];
+        //    array.resize(ref mas, mas.length - 2);
+        //    i -= 2;
+        //}
         public double Evalute(string output)
         {
             string result;
---
+++ calc/StringCalc.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class StringCalc
+    {
+        Operation op= new Operation();
+        public StringCalc()
+        {
+
+        }
+        public double? _result;
+
+        private double? Result
+        {
+            set
+            {
+                try
+                {
+                    _result = value;
+                }
+                catch (Exception er)
+                {
+                    Console.Error.WriteLine("Ошибка: " + er.Message);
+                    //Console.WriteLine("Ошибка: " + er.Message);
+                }
+            }
+            get
+            {
+                return _result;
+            }
+        }
+
+        public void Calculate(string input)
+        {
+            string output = GetExpression(input);
+            _result = op.Evalute(output);
+        }
+
+        public string GetExpression(string input)
+        {
+            string output = string.Empty;
+
+            Stack<char> operStack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Is_Delimeter(input[i]))
+                    continue;
+
+                if (input[i] == '-' && ((i > 0 && !Char.IsDigit(input[i - 1])) || i == 0))
+                {
+                    i++;
+                    output += "-";
+                }
+
+                if (Char.IsDigit(input[i]))
+                {
+                    while (!Is_Delimeter(input[i]) && !(op.operations.IndexOf(input[i]) != -1))
+                    {
+                        output += input[i];
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    output += " ";
+                    i--;
+                }
+
+                if (op.operations.IndexOf(input[i]) != -1)
+                {
+                    if (input[i] == '(')
+                        operStack.Push(input[i]);
+                    else if (input[i] == ')')
+                    {
+                        char s = operStack.Pop();
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        if (operStack.Count > 0)
+                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek()))
+                                output += operStack.Pop().ToString() + " ";
+                        operStack.Push(char.Parse(input[i].ToString()));
+                    }
+                }
+            }
+
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+            return output;
+        }
+
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                default: return 5;
+            }
+        }
+
+        static private bool Is_Delimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+    }
+}
