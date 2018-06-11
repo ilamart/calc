cd903d3b9b1e2332066919ee68b82d0a91c689b1
ilamart <39222202+ilamart@users.noreply.github.com>
2018-06-11 17:20:11 +0300

    Update StringCalc.cs

--- calc/StringCalc.cs
+++ calc/StringCalc.cs
@@ -1,33 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace calc
 {
     class StringCalc
     {
-        Operation op= new Operation();
+        public string operations = "+-/*^()%";
+        //убрать строковую константу
+
+        private static Operation[] _operations = { new OperationPlus(), new OperationMinus(), new OperationMultiply(), new OperationDivide(), new OperationExpone(), new OperationMyDivide() };
+
         public StringCalc()
         {
-
         }
-        public double? _result;
+        public double _result;
 
-        private double? Result
+        private double Result
         {
             set
             {
-                try
-                {
                     _result = value;
-                }
-                catch (Exception er)
-                {
-                    Console.Error.WriteLine("Ошибка: " + er.Message);
-                    //Console.WriteLine("Ошибка: " + er.Message);
-                }
             }
             get
             {
@@ -38,19 +31,17 @@ namespace calc
         public void Calculate(string input)
         {
             string output = GetExpression(input);
-            _result = op.Evalute(output);
+            _result = Analysis(output);
         }
-
+        
         public string GetExpression(string input)
         {
             string output = string.Empty;
-
             Stack<char> operStack = new Stack<char>();
             for (int i = 0; i < input.Length; i++)
             {
                 if (Is_Delimeter(input[i]))
                     continue;
-
                 if (input[i] == '-' && ((i > 0 && !Char.IsDigit(input[i - 1])) || i == 0))
                 {
                     i++;
@@ -59,7 +50,7 @@ namespace calc
 
                 if (Char.IsDigit(input[i]))
                 {
-                    while (!Is_Delimeter(input[i]) && !(op.operations.IndexOf(input[i]) != -1))
+                    while (!Is_Delimeter(input[i]) && !(operations.IndexOf(input[i]) != -1))
                     {
                         output += input[i];
                         i++;
@@ -69,7 +60,7 @@ namespace calc
                     i--;
                 }
 
-                if (op.operations.IndexOf(input[i]) != -1)
+                if (operations.IndexOf(input[i]) != -1)
                 {
                     if (input[i] == '(')
                         operStack.Push(input[i]);
@@ -91,7 +82,6 @@ namespace calc
                     }
                 }
             }
-
             while (operStack.Count > 0)
                 output += operStack.Pop() + " ";
             return output;
@@ -106,8 +96,10 @@ namespace calc
                 case '+': return 2;
                 case '-': return 3;
                 case '*': return 4;
-                case '/': return 4;
-                default: return 5;
+                case '/': return 5;
+                case '^': return 6;
+                case '%': return 7;
+                default: return 8;
             }
         }
 
@@ -116,6 +108,33 @@ namespace calc
             if ((" =".IndexOf(c) != -1))
                 return true;
             return false;
+        }
+
+        public double Analysis(string output)
+        {
+            List<string> list = new List<string>(output.Split(' '));
+            foreach (var s in list)
+            {
+                Console.WriteLine(s + " ");
+            } 
+            double DubResult;
+
+            for (int i = 0; i < (list.Count); i++)
+            {
+                foreach (Operation item in _operations)
+                {
+                    if (item.Code == list[i])
+                    {
+                        DubResult = item.Evaluate(double.Parse(list[i - 2]), double.Parse(list[i - 1]));
+                        list[i - 2] = DubResult.ToString();
+                        for (int j = i - 1; j < list.Count - 2; j++)
+                            list[j] = list[j + 2];
+                        list.RemoveRange(list.Count - 2,2);
+                        i -= 2;
+                    }
+                }
+            }
+            return double.Parse(list[0]);
         }
     }
 }
